34e4690a50de3065b7e2f35ff0ccd91bb9bd6b40
svenmkrueger <svenmkrueger@gmx.de>
2026-09-10 09:42:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Fassung behielten — hier gibt es nur noch eine Quelle.
  * ========================================================================== */
 
-const CACHE = "handhoch-v0.21.1";
+const CACHE = "handhoch-v0.22.2";
 const DATEIEN = [
   "./",
   "./index.html",
